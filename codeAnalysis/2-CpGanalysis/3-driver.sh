e976b4eeb2fb26a81f112565a262271181cb3ec9
cbreenmachine <coleman.breen@wisc.edu>
2022-12-21 09:17:40 -0600

    ported old code, streamlined CpG analysis

--- codeAnalysis/2-CpGanalysis/3-driver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-effect_range="0 0.1 0.25 0.5 0.75"
-upstream_range="0"
-
-parallel --jobs 5 \
-    Rscript 3-callDMGenes.R \
-    --effect_cut {1} --upstream {2} \
-    ::: $effect_range ::: $upstream_range
